14a3880877b1fff00057c5e35f4cc2866928fce9
Rob Miller <rob@bigfish.co.uk>
2020-12-02 16:41:56 +0000

    Initial commit
    `bundle gem jfm`

---
+++ lib/jfm.rb
@@ -0,0 +1,6 @@
+require "jfm/version"
+
+module Jfm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jfm_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Jfm do
+  it "has a version number" do
+    expect(Jfm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
