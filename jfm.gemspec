9afe4a6ce7fcc9b6fce955b05a9fdda709010224
Rob Miller <rob@bigfish.co.uk>
2020-12-02 17:47:48 +0000

    First command: find
    jfw find will match posts that match a given query, outputting a list of
    filenames.

--- jfm.gemspec
+++ jfm.gemspec
@@ -14,6 +14,8 @@ Gem::Specification.new do |spec|
   spec.metadata["homepage_uri"] = spec.homepage
   spec.metadata["source_code_uri"] = "https://github.com/robmiller/jfm"
 
+  spec.add_runtime_dependency "dry-cli", "~> 0.6.0"
+
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
   spec.files         = Dir.chdir(File.expand_path('..', __FILE__)) do
--- lib/jfm.rb
+++ lib/jfm.rb
@@ -1,6 +1,61 @@
+require "pathname"
+require "yaml"
+
 require "jfm/version"
 
 module Jfm
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Site
+    def initialize(dir)
+      @working_dir = Pathname(dir)
+    end
+
+    def posts
+      post_files.map { |file| Post.new(file) }
+    end
+
+    private
+
+    def post_files
+      Dir[@working_dir + "_posts" + "*"]
+    end
+  end
+
+  class Post
+    attr_reader :filename
+
+    def initialize(filename)
+      @filename = filename
+    end
+
+    def frontmatter
+      YAML.load(raw_frontmatter)
+    end
+
+    def frontmatter=(new_frontmatter)
+      @frontmatter = new_frontmatter
+    end
+
+    def match?(query)
+      case query
+      when /(.+?):\s*~(.+)/
+        frontmatter[$1] != $2
+      when /(.+?):\s*(.+)/
+        frontmatter[$1] == $2
+      else
+        frontmatter.has_key?(query)
+      end
+    end
+
+    private
+
+    def content
+      @content ||= File.read(filename)
+    end
+
+    def raw_frontmatter
+      content.lines.drop_while { |l| l.chomp == "---" }.take_while { |l| l.chomp != "---" }.join
+    end
+  end
 end
